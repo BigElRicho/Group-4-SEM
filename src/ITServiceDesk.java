550a53a5628ff1c423c7e98e735b430f7df3eb8d
Julian Rich <julian_rich@hotmail.com>
2021-04-07 21:48:10 +1000

    strange issue fixed.

--- src/ITServiceDesk.java
+++ src/ITServiceDesk.java
@@ -1,4 +1,5 @@
 class ITServiceDesk{
+
     public static void main (String[] args){
         String welcomeMessage = "Hello, this is the IT Service Desk.";
         System.out.println(welcomeMessage);
---
+++ src/TechnicianLevelOne.java
@@ -0,0 +1,64 @@
+public class TechnicianLevelOne implements TechnicianInterface{
+
+    //Attributes
+    int numberOfTicketsCurrentlyAssigned = 0;
+    int technicianLevel = 1;
+    String userName = "";
+    String password = "";
+
+    //Constructors
+    public TechnicianLevelOne(int startingTicktes, int level, String userName, String password){
+        this.numberOfTicketsCurrentlyAssigned = startingTicktes;
+        this.technicianLevel = level;
+        this.userName = userName;
+        this.password = password;
+    }
+
+    //Methods
+    @Override
+    public String escalateIssue() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String deescalateIssue() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String openIssue() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String closeIssueWithoutResolution() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String closeAndResolveIssue() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public String changeUserName(){
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public String changePassword(){
+        //TODO
+        return null;
+    }
+
+    @Override
+    public String changeUsername() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
